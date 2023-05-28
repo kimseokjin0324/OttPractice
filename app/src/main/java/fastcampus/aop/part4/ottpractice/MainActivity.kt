66510a8ebe5c3a49efb28a3c129e3ac1073a430e
kimseokjin0324 <kimseokjin0324@gmail.com>
2023-05-28 19:54:47 +0900

    AppBarLayout을이용한 헤더 애니메이션 구현

--- app/src/main/java/fastcampus/aop/part4/ottpractice/MainActivity.kt
+++ app/src/main/java/fastcampus/aop/part4/ottpractice/MainActivity.kt
@@ -1,12 +1,21 @@
 package fastcampus.aop.part4.ottpractice
 
+import android.app.Activity
 import android.content.Context
+import android.graphics.Color
 import android.os.Bundle
 import android.util.TypedValue
+import android.view.View
+import android.view.ViewGroup
+import android.view.WindowManager
 import androidx.appcompat.app.AppCompatActivity
 import fastcampus.aop.part4.ottpractice.databinding.ActivityMainBinding
 import androidx.constraintlayout.motion.widget.MotionLayout
 import androidx.constraintlayout.motion.widget.MotionLayout.TransitionListener
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.appbar.AppBarLayout
+import java.lang.Math.abs
 
 class MainActivity : AppCompatActivity() {
 
@@ -17,8 +26,64 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
 
+        makeStatusBarTransparent()
+
+        initAppBar()
+
+        initInsetMargin()
+        initScrollViewListeners()
+
+        initMotionLayoutListeners()
+    }
+
+    private fun initAppBar() {
+        binding.appBar.addOnOffsetChangedListener(AppBarLayout.OnOffsetChangedListener { appBarLayout, verticalOffset ->
+            val topPadding = 120f.dpToPx(this)
+            val abstractOffset = abs(verticalOffset)
+            if (abstractOffset < topPadding) {
+                binding.toolbarBackgroundView.alpha = 0f //-투명도가 0
+                return@OnOffsetChangedListener
+            }
+            val verticalOffsetByTopPadding = abstractOffset - topPadding
+            val percentage = abs(verticalOffsetByTopPadding) / appBarLayout.totalScrollRange
+            binding.toolbarBackgroundView.alpha = 1 - (if (1 - percentage * 2 < 0) 0f else 1 - percentage * 2) //리스너를 통해서 앱바의 투명도가 1까지 변경됨
+        })
+        initActionBar()
+    }
+
+    private fun initActionBar() = with(binding) {
+        toolbar.navigationIcon = null
+        toolbar.setContentInsetsAbsolute(0, 0)
+        setSupportActionBar(binding.toolbar)
+        supportActionBar?.let {
+            it.setHomeButtonEnabled(false)
+            it.setDisplayHomeAsUpEnabled(false)
+            it.setDisplayShowHomeEnabled(false)
+        }
+    }
+
+    private fun initInsetMargin() = with(binding) {
+        ViewCompat.setOnApplyWindowInsetsListener(coordinator) { v: View, insets: WindowInsetsCompat ->
+            val params = v.layoutParams as ViewGroup.MarginLayoutParams
+            params.bottomMargin = insets.systemWindowInsetBottom
+            toolbarContainer.layoutParams = (toolbarContainer.layoutParams as ViewGroup.MarginLayoutParams).apply {
+                setMargins(0, insets.systemWindowInsetTop, 0, 0)
+            }
+            collapsingToolbarContainer.layoutParams = (collapsingToolbarContainer.layoutParams as ViewGroup.MarginLayoutParams).apply {
+                setMargins(0, 0, 0, 0)
+            }
+
+            insets.consumeSystemWindowInsets()
+        }
+    }
+
+    private fun initScrollViewListeners() {
+        binding.scrollView.smoothScrollTo(0, 0)
+
         binding.scrollView.viewTreeObserver.addOnScrollChangedListener {
-            if (binding.scrollView.scrollY > 150f.dpToPx(this@MainActivity).toInt()) {
+            val scrolledValue = binding.scrollView.scrollY
+
+            if (scrolledValue > 150f.dpToPx(this@MainActivity).toInt()) {
                 if (isGateringMotionAnimating.not()) {
                     binding.gatheringDigitalThingsMotionLayout.transitionToEnd()
                     binding.buttonShownMotionLayout.transitionToEnd()
@@ -30,7 +95,9 @@ class MainActivity : AppCompatActivity() {
                 }
             }
         }
+    }
 
+    private fun initMotionLayoutListeners() {
         binding.gatheringDigitalThingsMotionLayout.setTransitionListener(object : TransitionListener {
             override fun onTransitionStarted(p0: MotionLayout?, p1: Int, p2: Int) {
                 isGateringMotionAnimating = true
@@ -46,7 +113,17 @@ class MainActivity : AppCompatActivity() {
 
         })
     }
+
 }
 
 fun Float.dpToPx(context: Context): Float =
     TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, this, context.resources.displayMetrics)
+
+fun Activity.makeStatusBarTransparent() {
+    window.apply {
+        decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN or
+                View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+        addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS)
+        statusBarColor = Color.TRANSPARENT
+    }
+}
